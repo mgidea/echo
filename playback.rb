c16b9b2cfedfd318fff47838c8f106571d318962
Tom Corley <tomcorleycarpentry@gmail.com>
2013-08-22 15:45:03 -0400

    finish phase 3

--- playback.rb
+++ playback.rb
@@ -1,10 +1,26 @@
+statements = []
 puts "what do you want to say?"
 print "> "
 reply = gets.chomp
   if reply == 'Nothing!'
     puts "Ok, fine!"
   elsif reply == 'I have a lot to say'
-    puts "I don't have time for that right now!"
+    puts "Ok, let's hear it!"
+    until statements.include?("done")
+     statements << gets.chomp
+    end
+      statements.pop
+      statements.each_with_index do |statement, index|
+        if index == 0
+          puts "You said: #{statement}"
+        elsif index == statements.size - 1
+          puts "Finally you said: #{statement}"
+        else
+          puts "Then, you said: #{statement}"
+        end
+      end
+      puts "Phew! Glad you got all #{statements.size} of those things off your chest!"
+
   else
     puts "You said: #{reply}"
   end

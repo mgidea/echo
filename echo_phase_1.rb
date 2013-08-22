48ee3068d205d2d52b4a92e33941da33f1ffeb7e
Tom Corley <tomcorleycarpentry@gmail.com>
2013-08-22 15:00:08 -0400

    finished first stage

---
+++ echo_phase_1.rb
@@ -0,0 +1,12 @@
+
+  puts "what do you want to say?"
+  print "> "
+  reply = gets.chomp
+  puts "#{reply}"
+  puts "You said: #{reply}"
+
+def playback response
+  puts "You said: #{response}"
+end
+statement = "Sally sells seashells"
+playback(statement)

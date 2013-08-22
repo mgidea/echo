583c9899c273e4adf48575fdc162218e9fad9c9e
Tom Corley <tomcorleycarpentry@gmail.com>
2013-08-22 15:09:02 -0400

    Echo phase II complete

--- playback.rb
+++ playback.rb
@@ -1,1 +1,18 @@
+puts "what do you want to say?"
+print "> "
+reply = gets.chomp
+  if reply == 'Nothing!'
+    puts "Ok, fine!"
+  elsif reply == 'I have a lot to say'
+    puts "I don't have time for that right now!"
+  else
+    puts "You said: #{reply}"
+  end
+
+
+# def playback response
+#   puts "You said: #{response}"
+# end
+# statement = "Sally sells seashells"
+# playback(statement)
 

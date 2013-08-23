0b7fa8cd17e43410bdd3a737a9fe2112c0a1bc0b
Kevin Sun <sunkev@brandeis.edu>
2013-08-23 09:54:25 -0400

    cleaning

--- copy5.rb
+++ copy5.rb
@@ -1,38 +1,6 @@
 statements = []
-puts "what do you want to say?"
-print "> "
-reply = gets.chomp
 
-if reply == 'Nothing!'
-  puts "Ok, fine!"
-elsif reply == 'I have a lot to say'
-  puts "Ok, let's hear it!"
-  until statements.include?("done")
-   statements << gets.chomp
-  end
-  statements.pop
-  statements.each_with_index do |statement, index|
-    if index == 0
-    puts "You said: #{statement}"
-    elsif index == statements.size - 1
-    puts "Finally you said: #{statement}"
-    else
-    puts "Then, you said: #{statement}"
-    end
-  end
-    puts "Phew! Glad you got all #{statements.size} of those things off your chest!"
-elsif reply == "I have something prepared"
-  puts "Ok, where can I find what you want to say?"
-  your_file = gets.chomp
-  while File.file?(your_file) == false
-    puts "I couldn't find that file"
-    puts "Where can I find what you want to say?"
-    your_file = gets.chomp
-  end
-
-  File.open(your_file, 'r').each_line do |line|
-    statements << line
-  end
+def puts_ordered_statements(statements)
   statements.each_with_index do |statement, index|
     if index == 0
       puts "You said: #{statement}"
@@ -43,6 +11,39 @@ elsif reply == "I have something prepared"
     end
   end
   puts "Phew! Glad you got all #{statements.size} of those things off your chest!"
+end
+
+def check_valid_file(file)
+  while File.file?(file) == false
+    puts "I couldn't find that file"
+    puts "Where can I find what you want to say?"
+    file = gets.chomp
+  end
+end
+
+puts "what do you want to say?"
+print "> "
+reply = gets.chomp
+case reply
+when 'Nothing!'
+  puts "Ok, fine!"
+when 'I have a lot to say'
+  puts "Ok, let's hear it!"
+  until statements.include?("done")
+    statements << gets.chomp
+  end
+  statements.pop
+  puts_ordered_statements(statements)
+when "I have something prepared"
+  puts "Ok, where can I find what you want to say?"
+  your_file = gets.chomp
+
+  check_valid_file(your_file)
+
+  File.open(your_file, 'r').each_line do |line|
+    statements << line
+  end
+  puts_ordered_statements(statements)
 else
   puts "You said: #{reply}"
 end
